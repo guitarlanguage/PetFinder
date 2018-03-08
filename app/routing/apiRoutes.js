3a3927ea9868297a85513230ee0b185567ff650e
Tom LaChance <guitarlanguageseries@gmail.com>
2018-03-08 16:32:59 -0600

    apiRoutes now totals the user's inputted scores

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,11 +1,6 @@
 // var path = require('path');
 var siteFriends = require ("../data/friends.js");
 
-// 4. Your `apiRoutes.js` file should contain two routes:
-//
-//    * A GET route with the url `/api/friends`.
-//    This will be used to display a JSON of all possible friends.
-
 //    * A POST routes `/api/friends`. This will be used to handle
 //  incoming survey results. This route will also be used to handle
 //   the compatibility logic.
@@ -18,7 +13,7 @@ module.exports = function(app) {
 
     app.post("/api/friends", function (req, res) {
         //logic goes here---------->
-        // console.log(req);
+        console.log(req);
         var totalScore = 0;
         var userBio = req.body;
         var userScore = userBio['scores[]'];
@@ -26,8 +21,19 @@ module.exports = function(app) {
         siteFriends.push(userBio);
 
         userScore.forEach(function(elem, i) {
-            console.log(`here I am: ${elem}`);
-        })
+            totalScore += Math.abs(elem);
+            console.log(`
+
+                here I am: ${Math.abs(elem)};`);
+
+        });
+
+        console.log(parseInt(totalScore));
+        // console.log(sightFriends);
+        // siteFriends.forEach(function(elem, i) {
+        //     console.log(`sightFriends: ${elem};`);
+        //
+        // })
 
     });
 
