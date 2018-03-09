37d507944718a5963e297769a29e515d81e52674
Tom LaChance <guitarlanguageseries@gmail.com>
2018-03-08 22:51:44 -0600

    changed survey.html  from a $.post request to an ajax request with traditional: true, this fixed the syntax.  apiRoutes now has a loop within a loop to target scores and do processing.

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -14,26 +14,47 @@ module.exports = function(app) {
     app.post("/api/friends", function (req, res) {
         //logic goes here---------->
         // console.log(req.body);
-        var membersScores = 0;
+        // console.log(res.body);
         var totalScore = 0;
         var userBio = req.body;
-        var userScore = userBio['scores[]'];
+        // var userScore = userBio.scores;
+        // var memberScores = siteFriends.scores;
         // console.log(userScore);
         siteFriends.push(userBio);
 
-        // console.log(siteFriends);
+        console.log(siteFriends[0].scores);
+        // console.log(`___________`);
+        // console.log(userScore);
 
         siteFriends.forEach(function(element, j) {
             // membersScores += Math.abs(element.res.json);
-            console.log(`${element.name}: ${element.scores}`);
+            console.log(`${element.name}: ${element.scores} with an index of ${j}`);
+            // totalScore += Math.abs(siteFriends[j].scores);
+            //targeting each score sheet
+            // console.log(siteFriends[j].scores);
+
+            siteFriends[j].scores.forEach(function(item, k) {
+                item
+                console.log(item);
+            })
+            // console.log(element.scores);
+
+            // membersScores += Math.abs(element.scores);
         })
-
-        // userScore.forEach(function(elem, i) {
-        //     totalScore += Math.abs(elem);
-        //     console.log(`scores: ${elem} and index: ${i}`);
+        // console.log(totalScore);
+        // console.log(membersScores);
+        // siteFriends.scores.forEach(function(element, j) {
+        //     // membersScores += Math.abs(element.res.json);
         //
-        // });
-
+        //
+        // })
+        // console.log(`${siteFriends.name} score is: ${membersScores}`);
+    //     userScore.forEach(function(elem, i) {
+    //         totalScore += Math.abs(elem);
+    //         console.log(`scores: ${elem} and index: ${i}`);
+    //
+    //     });
+    // //
     // console.log(`${req.body.name}'s total score: ...${totalScore}`);
 
 
