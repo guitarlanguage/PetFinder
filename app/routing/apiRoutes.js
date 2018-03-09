9f0150c65883f93ae8268053e2610bc3314b4963
Tom LaChance <guitarlanguageseries@gmail.com>
2018-03-09 15:45:24 -0600

    only using two loops that correspond directly with siteFriends.  totalScore at the end still gives a value of NaN

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -2,10 +2,6 @@
 var siteFriends = require ("../data/friends.js");
 // var fs = require('fs');
 
-//    * A POST routes `/api/friends`. This will be used to handle
-//  incoming survey results. This route will also be used to handle
-//   the compatibility logic.
-
 module.exports = function(app) {
     //app represents express
     app.get("/api/friends", function (req, res) {
@@ -24,49 +20,59 @@ module.exports = function(app) {
 
     app.post("/api/friends", function (req, res) {
         //logic goes here---------->
-        // console.log(req.body);
         // console.log(res.body);
         var totalScore = 0;
         var userBio = req.body;
         var userScore = userBio.scores;
-        // var memberScores = siteFriends.scores;
         
         siteFriends.push(userBio);
-       
-        //testing trying to grab somebody
-        // console.log(siteFriends[0].scores);
-        // console.log(`___________`);
-        console.log(`userScore: ${userScore}`);
+        console.log(`userScore outside of loop: ${userScore}`);
 
         siteFriends.forEach(function(element, j) {
             var difference = 0;
-            // console.log(element);
+            console.log(`inside ${element.name}'s loop ******${j}*******`);
             console.log(`element.name: ${element.name}: element.scores:${element.scores} with a j index of ${j}`);
             // var scoreHolder = element[j].scores;
             // totalScore += Math.abs(siteFriends[j].scores);
             //targeting each score sheet
             // console.log(siteFriends[j].scores);
             // siteFriends[j].scores.forEach
+            
             element.scores.forEach(function(list, l) {
-                console.log(`--------`);
-                console.log(list);
-                console.log(`--------`);
+                
+                console.log(`==${list} ==== at ${l} index`);
+                console.log(`orbit userScore: ${userScore[l]}`);
+                console.log(`orbit userScore[l]: ${userScore[l]} - ${list} (list): ${userScore[l] - list}`);
+                totalScore += totalScore + userScore[l] - list;
+                console.log(`-------------`);
 
-                userScore.forEach(function(item, k) {
-                    var doMath = Math.abs(parseInt(item - list));
-                    console.log(`${element.name}'s (item): ${item} - (list): ${list}: ${doMath} | with a k index of ${k}`);
-                    console.log(`k index on second loop: ${k} and element.scores: ${element.scores}`);
-                    console.log(`___________________`)
-                    totalScore += Math.abs(parseInt(item - list));
-                    
-                })
             })
+            console.log(totalScore);
+                // userScore.forEach(function(item, k) {
+
+
+                //     // totalScore += MATH.abs(parseInt(siteFriends[j].scores))
+                //     //want to subtract item from a slowed down siteFriends[j].scores
+                //     console.log(`inside userScore's orbit: ${item} at an index of ${k}`);
+                //     console.log(siteFriends[j].scores);
+
+                //     // for (var c = 0; c < element.scores.length; c++) {
+
+                //     //     console.log(`inside element.scores.lenght's loop ******************`);
+                //     //     console.log(`current score for this orbit: ${element.scores[c]}`);
+                        
+
+                //     // }
+                    
+                    
+                // })
+            
 
             
-            console.log(`totalScore: ${totalScore}`)
+            console.log(`totalScore: ${totalScore}`);
 
             // membersScores += Math.abs(element.scores);
-        })
+        });
         // console.log(totalScore);
         // console.log(membersScores);
         // siteFriends.scores.forEach(function(element, j) {
