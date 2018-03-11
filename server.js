d9484d781cfaa7d6cbbc0c4520763772b7d88111
Tom LaChance <guitarlanguageseries@gmail.com>
2018-03-11 18:30:47 -0500

    changed the port to give parameter process.env.PORT

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var router = express.Router();
 // Sets up the Express App
 // =============================================================
 var app = express();
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.urlencoded({ extended: false }));
